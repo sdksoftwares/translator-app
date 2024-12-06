9773839f10f17902d78b448765e38312bef9a7ec
sdksoftwares <sdkminor@gmail.com>
2024-12-06 11:52:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
 from flask import Flask, request, jsonify, render_template
-from googletrans import Translator
+from deep_translator import GoogleTranslator
 
 app = Flask(__name__)
-translator = Translator()
 
 @app.route('/')
 def index():
@@ -19,11 +18,12 @@ def translate():
         if not text or not target_lang:
             return jsonify({'error': 'Invalid input'}), 400
 
-        translated = translator.translate(text, dest=target_lang)
-        return jsonify({'translated_text': translated.text})
+        # Use deep-translator for translation
+        translated_text = GoogleTranslator(source='auto', target=target_lang).translate(text)
+        return jsonify({'translated_text': translated_text})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable or default to 5000
-    app.run(host='0.0.0.0', port=port, debug=False)  # Debug set to False for production
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
